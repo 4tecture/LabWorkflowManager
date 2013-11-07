85559f57012565f9e1d40cbc2d5e93a5d69c8c5f
Marc Müller <mmueller@4tecture.ch>
2013-11-07 16:46:07 +0100

    added missing files

---
+++ Src/LabWorkflowManager/Config/CustomSettingsBase.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Practices.Prism.Logging;
+using Microsoft.Practices.ServiceLocation;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabWorkflowManager.Config
+{
+    public class CustomSettingsBase : ConfigurationSection
+    {
+        private static Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.PerUserRoamingAndLocal);
+
+        protected ILoggerFacade Logger { get; set; }
+
+        public CustomSettingsBase()
+        {
+            this.Logger = SetupLogger();
+        }
+
+        public void Save()
+        {
+            if (config != null)
+            {
+                config.Save();
+            }
+        }
+
+        protected virtual ILoggerFacade SetupLogger()
+        {
+            ILoggerFacade logInstance = new NullLogger();
+            try
+            {
+                var tmplogInstance = ServiceLocator.Current.GetInstance<ILoggerFacade>();
+                if (tmplogInstance != null)
+                {
+                    logInstance = tmplogInstance;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            return logInstance;
+        }
+
+        protected static T GetCurrentSettings<T>(string sectionName) where T : CustomSettingsBase
+        {
+            return config.GetSection(sectionName) as T;
+        }
+
+        private class NullLogger : ILoggerFacade
+        {
+            public void Log(string message, Category category, Priority priority)
+            {
+
+            }
+        }
+
+    }
+}
